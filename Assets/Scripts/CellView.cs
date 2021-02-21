41160da40a1dafbc6ebc70f7baebb80ecd8aa136
alexandr.shelekhin <alexandr.shelekhin@matryoshka.com>
2021-02-21 16:56:01 +0700

    added view for grid cell

---
+++ Assets/Scripts/CellView.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System;
+using System.Collections.Generic;
+using Mirror;
+
+namespace NetworkTicTacToe {
+	public class CellView : NetworkBehaviour {
+		[Serializable]
+		public class CellSpriteContainer {
+			public CellState State;
+			public Sprite    Sprite;
+		}
+
+		public Image                     Image;
+		public Button                    Button;
+		public List<CellSpriteContainer> Sprites;
+
+		int _x;
+		int _y;
+
+		GameplayController _gameplayController;
+		
+		public void Init(GameplayController gameplayController, int x, int y) {
+			_x                  = x;
+			_y                  = y;
+			_gameplayController = gameplayController;
+		}
+		
+		public void UpdateView() {
+			var curState        = _gameplayController.GetCellState(_x, _y);
+			var spriteContainer = Sprites.Find(x => (x.State == curState));
+			Image.sprite        = spriteContainer?.Sprite;
+			Button.interactable = (curState == CellState.None);
+		}
+
+		[Command]
+		public void CmdPlaceCell(int x, int y) {
+			
+		}
+	}
+}
