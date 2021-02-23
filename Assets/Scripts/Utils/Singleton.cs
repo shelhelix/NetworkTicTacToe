2e16f40c9fca015cc46ebbecfb7a8751504d96c1
alexandr.shelekhin <ultrashel@gmail.com>
2021-02-23 15:37:04 +0700

    added a base singleton class

---
+++ Assets/Scripts/Utils/Singleton.cs
@@ -0,0 +1,14 @@
+﻿namespace NetworkTicTacToe.Utils {
+	public class Singleton<T> where T : new() {
+		public static T Instance {
+			get {
+				if ( _instance == null ) {
+					_instance = new T();
+				}
+				return _instance;
+			}
+		}
+
+		static T _instance;
+	}
+}
